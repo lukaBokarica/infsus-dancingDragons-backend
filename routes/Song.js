49736c5f6a059fd12fdb759a05b63e62d3a905e3
Luka Bokarica <luka.bokarica1999@gmail.com>
2022-05-22 17:20:45 +0200

    fixed song update

--- routes/Song.js
+++ routes/Song.js
@@ -3,7 +3,8 @@ import express from "express";
 import {
     createSong,
     getAllSongs,
-    deleteSong
+    deleteSong,
+    updateSong
 } from "../controllers/Song.js";
 
 const router=express.Router();
@@ -11,5 +12,6 @@ const router=express.Router();
 router.get('/all', getAllSongs);
 router.post('/add', createSong);
 router.delete('/:id/delete', deleteSong);
+router.post('/:id/update', updateSong);
 
 export default router;
